32a058fdf909024557a0ae1c03c3494c7e49084e
Farras Widya <farraswidyan@gmail.com>
2022-01-25 18:39:50 +0700

    base get data done

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,8 +1,20 @@
-const {Content,Comment,tag,User} = require('../models');
-const {verifyJWT} = require('../helpers/jwt');
+const { Content, Comment, tag, User } = require('../models');
+const { verifyJWT } = require('../helpers/jwt');
 
-class contentController{
+class contentController {
 
+  static async getAllData(req, res, next) {
+    try {
+      const allData = await Content.findAll({
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        }
+      })
+      res.status(200).json(allData)
+    } catch (err) {
+      next(err)
+    }
+  }
 }
 
 module.exports = contentController
