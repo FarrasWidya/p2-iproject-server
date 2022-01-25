cfaca6b478ef5cb1460a8d83655ddb569df4dc93
Farras Widya <farraswidyan@gmail.com>
2022-01-25 16:24:49 +0700

    config,DB Create until Migrate

---
+++ migrations/20220124121817-create-content.js
@@ -0,0 +1,62 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      upvote: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      downvote: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:{
+            tableName:'Users'
+          },
+          key:'id',
+          onUpdate:'cascade',
+          onDelete:'set null'
+        }
+      },
+      TagId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:{
+            tableName:'Tags'
+          },
+          key:'id',
+          onUpdate:'cascade',
+          onDelete:'set null'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contents');
+  }
+};
