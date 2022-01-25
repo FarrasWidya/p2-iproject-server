ea7fd5639c8d9852ea8901bf68ca592709107331
Farras Widya <farraswidyan@gmail.com>
2022-01-25 18:12:54 +0700

    register done

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,21 @@ const {compareHash} = require('../helpers/bcrypt');
 const {createJWT} = require('../helpers/jwt');
 
 class userController{
+  static async register(req, res, next) {
+    try {
+      const { email, password, username, avatar, gender, bio } = req.body
+      const registerForm = { email, password, username, avatar, gender, bio }
 
+      const newUser = await User.create(registerForm)
+      res.status(201).json({
+        id: newUser.id,
+        username: newUser.username
+      })
+
+    } catch (err) {
+      next(err)
+    }
+  }
 }
 
 module.exports = userController
